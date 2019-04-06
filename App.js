bb0fece468a9b78c6dc0b274814688405bdb5169
Greg Herbowicz <herbowicz@gmail.com>
2019-04-06 13:44:56 +0200

    with react navigation

--- App.js
+++ App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MyIcons from './MyIcons'
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import MyFlatList from './MyFlatList'
+import MyIcons from './MyIcons'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MyIcons />
-        <MyFlatList />
-      </View>
-    );
-  }
+class HomeScreen extends Component {
+    static navigationOptions = {
+        title: 'Home',
+    };
+
+    state = {
+        items: [{ id: 1, name: 'Foo' }, { id: 2, name: 'Bar' }, { id: 3 }],
+    };
+
+    render() {
+        return (
+            <View>
+                <Text>Home</Text>
+                {this.state.items.map(item => (
+                    <Button
+                        key={item.id}
+                        title={`Show item ${item.id}`}
+                        onPress={() => this.props.navigation.navigate('Item', item)}
+                    />
+                ))}
+                <Button key="MyFlatList" title="MyFlatList"
+                    onPress={() => this.props.navigation.navigate('MyFlatList', MyFlatList)} />
+                <Button key="MyIcons" title="MyIcons"
+                    onPress={() => this.props.navigation.navigate('MyIcons', MyIcons)} />
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const ItemScreen = ({ navigation }) => (
+    <View>
+        <Text>Item #{navigation.state.params.id}</Text>
+        <Text>Item #{navigation.getParam('id', '-')}</Text>
+        <Text>{navigation.getParam('unknownParam', 'Not found unknownParam')}</Text>
+        <Button title="Rabbit hole" onPress={() => navigation.push('Item', navigation.state.params)} />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+);
+ItemScreen.navigationOptions = ({ navigation }) => ({ title: `Top title for Item #${navigation.getParam('id')}` })
+
+const Navigator = createStackNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+        },
+        Item: ItemScreen,
+        MyFlatList,
+        MyIcons
+    },
+    {
+        initialRouteName: 'Home',
+    }
+);
+const AppContainer = createAppContainer(Navigator); // Create container using our navigator stack
+
+export default class App extends Component {
+    render() {
+        return <AppContainer />;
+    }
+}
